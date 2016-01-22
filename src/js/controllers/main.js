36d1336ebd18937738250932dea482a410db95d4
Jon-Erik Liw <jonerik.liw@gmail.com>
2016-01-22 13:03:37 +0100

    .

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -4,6 +4,7 @@
         '$scope', '$translate', '$cookies', 'fileManagerConfig', 'item', 'fileNavigator', 'fileUploader','logger',
         function($scope, $translate, $cookies, fileManagerConfig, Item, FileNavigator, FileUploader,logger) {
 
+            logger.start();
             $scope.config = fileManagerConfig;
             $scope.reverse = false;
             $scope.predicate = ['model.type', 'model.name'];
@@ -43,6 +44,7 @@
             };
 
             $scope.smartClick = function (item) {
+                logger.stop();
                 if (item.isFolder()) {
                     return $scope.fileNavigator.folderClick(item);
                 }
--- src/js/services/logger.js
+++ src/js/services/logger.js
@@ -1,26 +1,46 @@
 (function () {
     'use strict';
-    angular.module('FileManagerApp').service('logger', logger);
+    angular.module('FileManagerApp').factory('logger', logger);
 
     logger.$inject = ['toastr'];
     function logger(toastr) {
-        var source = new EventSource('http://navet-file-dev.jelastic.elastx.net/navet-file-api/resources/logs/events?orgId=12');
-        source.addEventListener('log-entry', function (e) {
-            var data = JSON.parse(e.data);
-            toastr.info(data.file, data.action);
-        });
         
-        source.addEventListener('open', function (e) {
-            toastr.info('Connection opened');
-        }, false);
+        var source = new EventSource('http://navet-file-dev.jelastic.elastx.net/navet-file-api/resources/logs/events?orgId=12');
+        
+        var service = {
+            start : start,
+            stop : stop
+        };
+        return service;
+        
+        function start() {
+            
+            source.addEventListener('log-entry', function (e) {
+                var data = JSON.parse(e.data);
+                toastr.info(data.file, data.action);
+            });
 
-        source.addEventListener('error', function (e) {
-            toastr.error('Connection error');
-            if (e.readyState === EventSource.CLOSED) {
-                // Connection was closed.
-                toastr.error('Connection closed');
-            }   
-        }, false);
+            
+            source.addEventListener('open', function (e) {
+                toastr.info('Connection opened');
+            }, false);
+            
+            source.addEventListener('close', function (e) {
+                toastr.info('Connection closed');
+            }, false);
+
+            source.addEventListener('error', function (e) {
+                toastr.error('Connection error');
+                if (e.readyState === EventSource.CLOSED) {
+                    // Connection was closed.
+                    toastr.error('Connection closed');
+                }
+            }, false);
+        }
+        
+        function stop() {
+            source.close();
+        }
     }
 })();
 
